8bd6b7081273cd9918fb5fbac73d2fbcaeaca7f2
Nazmul <nazmul@udel.edu>
2024-11-05 18:16:55 -0500

    finisheed About Card and Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { HeroSection } from './components/HeroSection';
 import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Card from './components/Card';
 
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-black tracking-tighter text-gray-200 antialiased">
       <Navbar />
       <HeroSection />
+      <About />
+      <Projects />
+      <Card />
     </main>
   );
 };
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,6 +12,12 @@ export const HeroSection = () => {
           alt={PROFILE.name}
           className="absolute inset-0 z-10 h-full w-full object-cover"></img>
         <div className="absolute inset-0 z-10 bg-gradient-to-b from-transparent from-60% to-black lg:from-30%"></div>
+        <div className="z-20 mx-4 max-w-3xl pb-20">
+          <h1 className="text-5xl font-semibold uppercase tracking-wide md:text-7xl">
+            {PROFILE.name}
+          </h1>
+          <p className="pt-2 font-semibold">{PROFILE.info}</p>
+        </div>
       </div>
     </>
   );
